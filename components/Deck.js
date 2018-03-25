e7d598a43cd928805c45e35e6220ecf5d7801f38
Arend Hosman <info@webbb-online.com>
2018-03-25 16:23:02 +0200

    Adds Deck component

---
+++ components/Deck.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import globalStyles from '../utils/styles'
+
+const Deck = () => (
+  <View style={globalStyles.viewChild}>
+    Deck
+  </View>
+)
+
+export default Deck
