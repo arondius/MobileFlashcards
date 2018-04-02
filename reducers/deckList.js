eba85d28d5ecfe0db1d505c9352768eea72fc656
Arend Hosman <info@webbb-online.com>
2018-04-02 21:42:00 +0200

    Removes unnecessary state property

--- reducers/deckList.js
+++ reducers/deckList.js
@@ -4,30 +4,18 @@ const defaultDeckListState = [
   {
     id: 1,
     title: 'Spanish vocabulary',
-    questions: [
-      1, 2, 3, 4, 5, 6, 7, 8, 9,
-    ],
   },
   {
     id: 2,
     title: 'French vocabulary',
-    questions: [
-      10, 11, 12, 13, 14, 15, 16,
-    ],
   },
   {
     id: 3,
     title: 'React patterns',
-    questions: [
-      16, 17, 18, 19,
-    ],
   },
   {
     id: 4,
     title: 'Redux patterns',
-    questions: [
-      20, 21, 22, 23, 24, 25, 26, 27,
-    ],
   },
 ]
 
@@ -38,7 +26,7 @@ function deckList(state = defaultDeckListState, action) {
     case SAVE_DECK:
       return [...state, { id, title: action.title, questions: [] }]
     default:
-      return [...state]
+      return state
   }
 }
 
