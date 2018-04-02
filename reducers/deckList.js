ad558a0facb3831683e85699f55c8cb927852889
Arend Hosman <info@webbb-online.com>
2018-04-02 14:34:08 +0200

    Code styling

--- reducers/deckList.js
+++ reducers/deckList.js
@@ -1,31 +1,31 @@
-defaultDeckListState = [
+const defaultDeckListState = [
   {
     id: 1,
     title: 'Spanish vocabulary',
     questions: [
-      1,2,3,4,5,6,7,8,9
-    ]
+      1, 2, 3, 4, 5, 6, 7, 8, 9,
+    ],
   },
   {
     id: 2,
     title: 'French vocabulary',
     questions: [
-      10,11,12,13,14,15,16
-    ]
+      10, 11, 12, 13, 14, 15, 16,
+    ],
   },
   {
     id: 3,
     title: 'React patterns',
     questions: [
-      16,17,18,19
-    ]
+      16, 17, 18, 19,
+    ],
   },
   {
     id: 4,
     title: 'Redux patterns',
     questions: [
-      20,21,22,23,24,25,26,27
-    ]
+      20, 21, 22, 23, 24, 25, 26, 27,
+    ],
   },
 ]
 
