3e2ac89c874388d75e1209f586b0c872dd9b2fe2
Arend Hosman <info@webbb-online.com>
2018-04-02 09:27:18 +0200

    Code styling

--- components/Deck.js
+++ components/Deck.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import globalStyles from '../utils/styles'
+import React from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 import { withNavigation } from 'react-navigation'
+import globalStyles from '../utils/styles'
 
-const Deck = ( {deck, questions, navigation} ) => {
+const Deck = ({ deck, questions, navigation }) => {
   const { params } = navigation.state
   const { title } = deck
   const numberOfQuestions = questions.length
@@ -18,7 +18,7 @@ const Deck = ( {deck, questions, navigation} ) => {
         <Text>Start {title} quiz</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        onPress={() => ( navigation.navigate('AddCardContainer', params) )}
+        onPress={() => (navigation.navigate('AddCardContainer', params))}
         style={globalStyles.btn}
       >
         <Text>Add a new card to {title}</Text>
