cc9eedf2be0541247d4cb8ff94e713821e138091
Arend Hosman <info@webbb-online.com>
2018-04-03 22:05:54 +0200

    Adds aSyncStorage call to get all Decks

--- utils/api.js
+++ utils/api.js
@@ -10,3 +10,10 @@ export function saveDeckToStorage(id, title) {
   }
   return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify(data))
 }
+
+export async function getDecksFromStorage() {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    .then(res => JSON.parse(res))
+}
+
+//AsyncStorage.removeItem(DECKS_STORAGE_KEY)
