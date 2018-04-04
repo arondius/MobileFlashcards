9be31ffe8c25303d73bf3839df3b46bbb450c99f
Arend Hosman <info@webbb-online.com>
2018-04-04 15:28:56 +0200

    Ups Font size and check single plural

--- components/DeckListItem.js
+++ components/DeckListItem.js
@@ -8,8 +8,8 @@ const DeckListItem = ({ deck, navigation, cards }) => {
   const { title } = deck
   return (
     <View style={[globalStyles.viewChild, globalStyles.card]}>
-      <Text style={globalStyles.text}>{title}</Text>
-      <Text style={globalStyles.text}>{`${cards.length} cards`}</Text>
+      <Text style={[globalStyles.text, { fontSize: 24 }]}>{title}</Text>
+      <Text style={globalStyles.text}>{cards.length} {cards.length > 1 ? 'cards': 'card'}</Text>
       <TouchableOpacity
         onPress={() => (navigation.navigate('DeckContainer', { deck, cards }))}
         style={globalStyles.btn}
