12876fd94f798392dafd66715902a96e505dfc03
Arend Hosman <info@webbb-online.com>
2018-04-04 15:29:28 +0200

    Code styling

--- components/DeckListItem.js
+++ components/DeckListItem.js
@@ -9,7 +9,7 @@ const DeckListItem = ({ deck, navigation, cards }) => {
   return (
     <View style={[globalStyles.viewChild, globalStyles.card]}>
       <Text style={[globalStyles.text, { fontSize: 24 }]}>{title}</Text>
-      <Text style={globalStyles.text}>{cards.length} {cards.length > 1 ? 'cards': 'card'}</Text>
+      <Text style={globalStyles.text}>{cards.length} {cards.length > 1 ? 'cards' : 'card'}</Text>
       <TouchableOpacity
         onPress={() => (navigation.navigate('DeckContainer', { deck, cards }))}
         style={globalStyles.btn}
